9768c96cb02020f97daf81546bc8a59f45408168
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-07-04 10:17:48 +0530

    header designed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,34 @@
 import "./Header.css";
 import logo from "../IMAGES/movix-logo.png"
+import SearchIcon  from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
-      <img src={logo} alt="" className="logo"/> Movix entertainment 
-    </span>
+ <>
+     <div className="header">
+        
+          <span className="title">
+            <img src={logo} alt="" className="logo" />
+            MOVIX
+          </span>
+      
+
+        <div className="Navbar-search">
+          <input placeholder="Search Here For Movie..." className="searchInput" />
+          <button className="searchBtn">
+            <SearchIcon size={1} />
+          </button>
+        </div>
+        <div className="Navbar-account">
+
+          <button className="signInBtn">
+            <AccountCircleIcon style={{color: "#39445a", marginRight:"5px"}} />
+            Sign In
+          </button>
+        </div>
+      </div>
+ </>
   );
 };
 
