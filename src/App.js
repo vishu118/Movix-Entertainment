4a670bddc63c7269c486db5937fb6f5e3a83fe96
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-07-05 12:07:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import SimpleBottomNavigation from "./Components/MainNav";
+
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">
-    
+      <SimpleBottomNavigation/>
       </div>
     </>
   );
